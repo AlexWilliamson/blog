a35da7777db84d1940ea87c76306ef99cc08b63d
Alex Williamson <alex.williamson85@gmail.com>
2022-08-24 13:47:11 +0100

    Add Tailwind

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admins
+  resources :testmodels
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root "posts#index"
