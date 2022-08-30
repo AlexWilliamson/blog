c13e84c56743f4affe00a7de5185c3fe4f65223b
Alex Williamson <alex.williamson85@gmail.com>
2022-08-30 15:35:48 +0100

    Create "Admin" namespace Moved existing posts/controllers/views to new Admin folders Amended routes for Admin use New layout for Admins and non-admin users

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root "posts#index"
-  resources :posts, only: [:new, :index, :create, :show, :edit, :destroy, :update]
+
+  namespace :admin do
+    resources :posts, only: [:new, :index, :create, :show, :edit, :destroy, :update]
+  end
+
 end
